cfc91b834cb041843afdba02f0c91f5f733a83e3
Vaibhav Khanna <vaibhav_k@mt.iitr.ac.in>
2021-09-28 13:55:35 +0530

    Use actual Database

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+
 # Create your views here.
-posts = [
-    {
-        "author" : "Vaibhav Khanna",
-        "title" : "Post 1",
-        "content" : "Content of 1st post",
-        "date_posted" : "27th Sept, 2021"
-    },
-    {
-        "author" : "Jake Peralta",
-        "title" : "Post 2",
-        "content" : "Content of 2nd post",
-        "date_posted" : "27th Sept, 2021"
-    }
-]
 def home(request):
     context={
-        "posts" : posts
+        "posts" : Post.objects.all()
     }
     return render(request, "blog/home.html", context)
 
